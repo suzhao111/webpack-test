de77efd466281b22e368db9e746329f59c39c4ea
tpsuzhao <suzhao@taipuauto.cn>
2021-03-10 16:17:33 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+'use strict';
+const path = require('path');
+module.exports = {
+    // entry: './src/index.js',
+    entry:{
+        index: './src/index.js',
+        search: './src/search.js',
+    }, 
+    output: {
+        path: path.join(__dirname, 'dist'),
+        // filename: 'bundle.js'
+        filename: '[name].js'
+    },
+    mode: 'production'
+}
